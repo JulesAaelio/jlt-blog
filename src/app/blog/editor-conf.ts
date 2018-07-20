e9e82db0e47340a5621b99c856efe78a27e64cfa
Jules LAURENT <jules.laurent@ynov.com>
2018-07-20 15:27:24 +0200

    unify editor and site style.

--- src/app/blog/editor-conf.ts
+++ src/app/blog/editor-conf.ts
@@ -1,5 +1,6 @@
 export const conf = {
   plugins: 'codesample',
   menubar: false,
-  toolbar: 'formatselect | bold italic underline | alignleft alignright aligncenter alignjustify | codesample'
+  toolbar: 'formatselect | bold italic underline | alignleft alignright aligncenter alignjustify | codesample',
+  content_css: '/styles.css',
 };
