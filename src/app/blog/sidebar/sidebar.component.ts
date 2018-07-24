c8d97a797c9b09a9b3e3765c581fea6459db82eb
Jules LAURENT <jules.laurent@ynov.com>
2018-07-24 13:43:37 +0200

    add sidebar component

---
+++ src/app/blog/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../environments/environment';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  avatar_url = environment.rest_end_point + '/public/avatar.png';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
