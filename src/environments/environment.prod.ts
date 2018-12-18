6871474ffc022e764caa8ca1f1b4373dfa259af8
Jules LAURENT <jules.laurent@ynov.com>
2018-12-18 09:25:03 +0100

    remove environment.prod.ts from vcs

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  rest_end_point: 'http://api.blog.juleslaurent.fr',
-  auth_end_point: 'http://api.juleslaurent.fr',
-  resume_rest_end_point: 'http://api.juleslaurent.fr',
-  default_resume: 'FIRST',
-  app_id: '123',
-  base_title: 'Jules LAURENT'
-};
