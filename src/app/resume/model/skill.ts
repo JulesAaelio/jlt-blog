51fed7cbbfbb9c169f9d1d7b44ddbae2e0d15ef6
JulesAaelio <jules.laurent@outlook.com>
2021-03-22 22:30:13 +0100

    Enhance skill detail page

--- src/app/resume/model/skill.ts
+++ src/app/resume/model/skill.ts
@@ -1,7 +1,10 @@
 import {SkillCategory} from './skill_category';
+import {Experience} from './experience';
 
 export class Skill {
   id: number;
   name: string;
   skill_category: SkillCategory;
+  experiences: Experience[];
+  realisations: any[];
 }
