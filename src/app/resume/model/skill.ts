b9b07bb0d8406c8edbde363e42e1d3253d75e154
JulesAaelio <jules.laurent@outlook.com>
2021-04-14 09:23:08 +0200

    Add skill description

--- src/app/resume/model/skill.ts
+++ src/app/resume/model/skill.ts
@@ -1,10 +1,12 @@
 import {SkillCategory} from './skill_category';
 import {Experience} from './experience';
+import {Realisation} from './realisation';
 
 export class Skill {
   id: number;
   name: string;
   skill_category: SkillCategory;
   experiences: Experience[];
-  realisations: any[];
+  realisations: Realisation[];
+  description: string;
 }
